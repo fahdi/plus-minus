bd96fcec68e7ec78b094091b96f070a7add21a58
Fahad Murtaza <info@fahdmurtaza.com>
2016-04-03 18:30:29 +0400

    Adding the C solution

---
+++ plus-minus.c
@@ -0,0 +1,33 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n;
+    float fr1=0,fr2=0,fr3=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       scanf("%d",&arr[arr_i]);
+    }
+    for(int arr_i = 0; arr_i < n; arr_i++){
+        if(arr[arr_i]>0) {
+            fr1++;
+        }
+        if(arr[arr_i]<0) {
+            fr2++;
+        }
+        if(arr[arr_i]==0) {
+            fr3++;
+        }
+    }
+    printf("%.6f\n", fr1/n );
+    printf("%.6f\n", fr2/n );
+    printf("%.6f\n", fr3/n );
+
+    return 0;
+}
